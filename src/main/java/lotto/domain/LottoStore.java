759b15db8fc967d93e3086e70c5990c9762e0a41
jun-pil park <iamfeel2@gmail.com>
2020-06-05 18:02:31 +0900

    temp

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -6,19 +6,24 @@ import java.util.List;
 public class LottoStore {
 
     private final static int LOTTO_PRICE_PER_ONE = 1000;
-/*
-    public static List<Lotto> buy(int price, List<String> manualLotto) {
+    private final List<LottoGenerator> lottoGenerators = new ArrayList<>();
+
+    public LottoStore(LottoGenerator lottoGenerator) {
+
+    }
+
+    public List<Lotto> buy(int price, ManualLottoMemo memo) {
         validatePrice(price);
-        LottoGenerator lottoGenerator= LottoGenerator.newInstance();
-        int count = (int) Math.floor(price / LOTTO_PRICE_PER_ONE);
-        return lottoGenerator.generator(count);
-        return new ArrayList<Lotto>();
+
+    }
+
+    private int getPossibleBuyCount(int price) {
+
     }
 
     private static void validatePrice(int price) {
         if(price < LOTTO_PRICE_PER_ONE) {
             throw new IllegalArgumentException("입력된 금액이 로또구매 최소금액보다 작습니다.");
         }
-    }*/
-
+    }
 }
